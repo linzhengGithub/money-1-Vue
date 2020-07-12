0bf9192e2ec24dc4f691efb5ce2c0087fed088ca
linzheng <alidlg9045@126.com>
2020-07-12 15:20:05 +0800

    完成 Money页面布局

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,6 +1,8 @@
 <template>
-  <div>
-    M
+  <div class="nav-wrapper">
+    <div class="content">
+      <p>M</p>
+    </div>
     <Nav/>
   </div>
 </template>
@@ -12,5 +14,15 @@
 </script>
 
 <style lang="scss" scoped>
-
+  .nav-wrapper{
+    border: 1px solid blue;
+    display: flex;
+    flex-direction: column;
+    height: 100vh;
+  }
+  .content{
+    border: 1px solid green;
+    flex-grow: 1;
+    overflow: auto;
+  }
 </style>
