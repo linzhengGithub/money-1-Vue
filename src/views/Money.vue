db0d8de60d7fc692e8f0a9149b2c74d53847826f
linzheng <alidlg9045@126.com>
2020-07-16 16:25:17 +0800

    完成 点击ok localStorage 保存到浏览器

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,7 +1,7 @@
 <template>
   <Layout class-prefix="layout">
-    <NumberPad @update:value="onUpdateAmount"/>
-    <Types :value="record.type" @update:value="onUpdateType"/>
+    <NumberPad @update:value="onUpdateAmount" @submit="saveRecord"/>
+    <Types @update:value="onUpdateType"/>
     <Notes @update:value="onUpdateNotes"/>
     <Tags :data-source.sync="tags" @update:value="onUpdateTags"/>
     {{record}}
@@ -14,7 +14,7 @@
   import Tags from '@/components/Money/Tags.vue';
   import Notes from '@/components/Money/Notes.vue';
   import Types from '@/components/Money/Types.vue';
-  import {Component} from 'vue-property-decorator';
+  import {Component, Watch} from 'vue-property-decorator';
 
   type Record = {
     type: string;
@@ -31,6 +31,7 @@
     record: Record = {
       tags: [], note: '', type: '-', amount: 0
     };
+    recordList: Record[] = [];
 
     onUpdateTags(value: string[]) {
       this.record.tags = value
@@ -46,6 +47,15 @@
 
     onUpdateAmount(value: string) {
       this.record.amount = parseFloat(value)
+    }
+    saveRecord(){
+      const record2 = JSON.parse(JSON.stringify(this.record))
+      this.recordList.push(record2)
+      console.log(this.record);
+    }
+    @Watch('recordList')
+    onRecordListChanged(){
+      window.localStorage.setItem('recordList',JSON.stringify(this.recordList))
     }
   }
 </script>
